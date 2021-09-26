4165014ed5b61cdaf4b66fcbab5cbb6e5e696798
yama-choki <lovepingpong0@gmail.com>
2021-09-26 22:25:29 +0900

    .envにprojectId等の記載を移動した

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,22 +1,17 @@
 import firebase from 'firebase'
 
-// const config = {
-//   projectId: process.env.FIREBASE_PROJECT_ID
-// }
+const config = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+}
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(
-    {
-      apiKey: 'AIzaSyC8nxIlMSGwhjtsnCPIJip03C4VF1wjwis',
-      authDomain: 'port-folio-forum.firebaseapp.com',
-      projectId: 'port-folio-forum',
-      storageBucket: 'port-folio-forum.appspot.com',
-      messagingSenderId: '1072993632283',
-      appId: '1:1072993632283:web:88fe6fe5b915bda0d378be',
-      measurementId: 'G-5525T0ELJ7'
-    }
-    // config
-  )
+  firebase.initializeApp(config)
 }
 
 export default firebase
